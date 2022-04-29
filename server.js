71c796d81a02a55ad4d310518f76e744a1166bc0
Joe Tang <joetang81@gmail.com>
2022-04-28 20:07:45 -0400

    connect database to the UI

--- server.js
+++ server.js
@@ -179,11 +179,16 @@ app.post("/urls/modify/:shortURL/:id", (req, res) => {
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const accountsRoutes = require("./routes/accounts");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/users", usersRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
+
+app.use("/accounts/", accountsRoutes(db));
+
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
