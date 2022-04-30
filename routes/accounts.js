0bc6ba2f9201f68392b881f1f5fb156ca0673649
Joe Tang <joetang81@gmail.com>
2022-04-30 13:52:41 -0400

    implement add new accounts

--- routes/accounts.js
+++ routes/accounts.js
@@ -23,14 +23,13 @@ module.exports = (db) => {
 
     db.query(queryString, values)
       .then(data => {
+        req.session.org = data.rows[0].org;
+        req.session.org_id = data.rows[0].org_id;
+
         const accounts = data.rows;
         const org = { name: data.rows[0].org, id: data.rows[0].org_id };
         const user = { id: req.params.id };
-
         const templateVars = { accounts: accounts, user: user, org: org };
-
-        req.session.org = data.rows[0].org;
-        req.session.org_id = data.rows[0].org_id;
 
         res.render("account", templateVars);
       })
@@ -41,8 +40,6 @@ module.exports = (db) => {
       });
 
   });
-
-
 
   router.get("/new", (req, res) => {
 
@@ -57,37 +54,97 @@ module.exports = (db) => {
   router.post("/new", (req, res) => {
 
     const queryString = `
-    INSERT INTO accounts (category_id, org_id, name, url, username, password) VALUES ($1, $2, $3, $4, $5, $6);
+      SELECT id
+      FROM categories
+      WHERE name ILIKE $1
     `;
 
-    //INSERT INTO accounts(category_id, org_id, name, url, username, password) VALUES(1, 1, 'Facebook', 'www.facebook.com', 'lighthouse_lab@gmail.com', 'l1234567');
+    const values = [req.body.category];
 
-    const values = [1, req.session.org_id, req.body.name, req.body.url, req.body.username, req.body.password];
+    db.query(queryString, values)
+      .then(data => {
+        if (data.rows[0]) {
+          console.log("Category exists, will use the category id returned to insert data ");
+
+          //To be removed: E.g. INSERT INTO accounts (category_id, org_id, name, url, username, password) VALUES (1, 1, 'Facebook', 'www.facebook.com', 'lighthouse_lab@gmail.com', 'l1234567');
+          const queryString1 = `
+          INSERT INTO accounts (category_id, org_id, name, url, username, password) VALUES ($1, $2, $3, $4, $5, $6);
+          `;
+          const values2 = [data.rows[0].id, req.session.org_id, req.body.name, req.body.url, req.body.username, req.body.password];
+
+          db.query(queryString1, values2)
+            .then(data => {
+              res.redirect('/accounts');
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+
+        }
+        else {
+          console.log("category doesn't exist, will insert new record to category table");
+
+          //To be removed: E.g. INSERT INTO categories (name) VALUES ('Social Media');
+          const queryString3 = `
+          INSERT INTO categories (name) VALUES ($1)
+          RETURNING id
+          `;
+          const values3 = [req.body.category];
+          console.log(values3);
+
+          db.query(queryString3, values3)
+            .then(data => {
+              const queryString4 = `
+              INSERT INTO accounts (category_id, org_id, name, url, username, password) VALUES ($1, $2, $3, $4, $5, $6);
+              `;
+              const values4 = [data.rows[0].id, req.session.org_id, req.body.name, req.body.url, req.body.username, req.body.password];
+
+              db.query(queryString4, values4)
+                .then(data => {
+                  res.redirect('/accounts');
+                })
+                .catch(err => {
+                  res
+                    .status(500)
+                    .json({ error: err.message });
+                });
+
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
+  });
+
+  router.post("/delete/:id", (req, res) => {
+
+    const queryString = `
+    DELETE FROM accounts WHERE ID = $1
+    `;
+
+    const values = [req.params.id];
     console.log(values);
 
-    res.redirect('/account');
-
-    // db.query(queryString, values)
-    //   .then(data => {
-    //     const accounts = data.rows;
-    //     const org = { name: data.rows[0].org, id: data.rows[0].org_id };
-    //     const user = { id: req.params.id };
-
-    //     const templateVars = { accounts: accounts, user: user, org: org };
-
-    //     req.session.user_id = req.params.id;
-    //     req.session.org = data.rows[0].org;
-    //     req.session.org_id = data.rows[0].org_id;
-
-    //     res.render("account", templateVars);
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
-
-
+    db.query(queryString, values)
+      .then(data => {
+        res.redirect('/accounts');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
 
   });
 
