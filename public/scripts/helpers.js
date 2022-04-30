5a07036646a8ad35d6b2c7083b0bb1e9c8880656
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-04-30 16:27:35 -0400

    Added popup screen with copied stuff

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,9 +1,17 @@
 const copyUsernameToClickBoard = (id) => {
+  const copyText = document.getElementById(id).innerHTML;
 // get the text from the DOM Element:
-navigator.clipboard.writeText(document.getElementById(id).innerHTML)
+navigator.clipboard.writeText(copyText);
+  /* Alert the copied text */
+navigator.clipboard.readText().then(
+    clipText => alert("Copied: " + clipText));
 }
 
 const copyPasswordToClickBoard = (id) => {
+  const copyText = document.getElementById(id).innerHTML;
   // get the text from the DOM Element:
-  navigator.clipboard.writeText(document.getElementById(id).innerHTML)
+  navigator.clipboard.writeText(copyText);
+    /* Alert the copied text */
+    navigator.clipboard.readText().then(
+      clipText => alert("Copied: " + clipText));
   }
