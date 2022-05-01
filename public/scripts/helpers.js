d67380b9ffd8c5afc8b71ac7c9e78a272d71d40b
Joe Tang <joetang81@gmail.com>
2022-04-30 20:24:20 -0400

    replace multiple files due to merge conflict from earlier PRs

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,17 +1,9 @@
 const copyUsernameToClickBoard = (id) => {
-  const copyText = document.getElementById(id).innerHTML;
 // get the text from the DOM Element:
-navigator.clipboard.writeText(copyText);
-  /* Alert the copied text */
-navigator.clipboard.readText().then(
-    clipText => alert("Copied: " + clipText));
+navigator.clipboard.writeText(document.getElementById(id).innerHTML)
 }
 
 const copyPasswordToClickBoard = (id) => {
-  const copyText = document.getElementById(id).innerHTML;
   // get the text from the DOM Element:
-  navigator.clipboard.writeText(copyText);
-    /* Alert the copied text */
-    navigator.clipboard.readText().then(
-      clipText => alert("Copied: " + clipText));
+  navigator.clipboard.writeText(document.getElementById(id).innerHTML)
   }
