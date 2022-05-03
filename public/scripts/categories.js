1725bf56c08738f06a0253daab1cb7d76c5cb1a1
Michael Buffone <m_buffone@hotmail.com>
2022-05-03 16:42:35 -0400

    This made the toggle categories work and removed unwanted classes and added class active for tab bar

--- public/scripts/categories.js
+++ public/scripts/categories.js
@@ -2,25 +2,32 @@ $(document).ready(function() {
 
   $('.tab-panel .tabs li').on('click', function() {
     // checks for the tab icon clicked
+
     const visiableTab = $(this).attr('class');
-    alert(visiableTab)
 
     //loops through all cards
     $('.cards').each(function() {
        // finds the index of each card
       const index = $(this).index('.cards');
 
-      if (visiableTab === All) {
-        $('.cards').eq(index).addClass('show');
-      }
-
       //finds the card from the index id
       if($('.cards').eq(index).attr('id') === visiableTab) {
-        $('.cards').eq(index).addClass('show');
+        // alert(index + $('.cards').eq(index).attr('id'))
+        $('.cards').eq(index).removeClass('hide');
       } else {
+        // $('.cards').eq(index).removeClass('show');
         $('.cards').eq(index).addClass('hide');
       }
     })
+
+    // this show which tab is active
+    $('.tab-panel .tabs li.active').removeClass('active');
+    $(this).addClass('active');
+
+    // to show all accounts
+    if(visiableTab === 'All') {
+      $('.cards').removeClass('hide');
+    }
 
   })
 
