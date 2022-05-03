99652c8b2ab65ad8efb6706b7cdafb3095da2494
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-05-03 01:15:16 -0400

    Add options to generator

--- public/scripts/newpass.js
+++ public/scripts/newpass.js
@@ -3,21 +3,31 @@ $(document).ready(function() {
   $('.alert').hide()
 
 
-const nymbers = "0123456789";
+const numbers = "0123456789";
 const symbols = "!@#$%^&*_+=?.,><|";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let password = "";
 // getting the value of our slider
 let passwordLength = document.getElementById("slider").value;
+const password = document.getElementById("password");
 const generate = document.getElementById("generate");
 const copy = document.getElementById("copy");
+// checking if any of the checkboxes are checked
+let checkedNumber = document.getElementById("number").checked;
+let checkedUpperCase = document.getElementById("upper").checked;
+let checkedLowerCase = document.getElementById("lower").checked;
+let checkedSymbol = document.getElementById("symbol").checked;
 
 
-// Add eventlistener on click at generate ID
+// Add eventlistener on click at 'generate' ID
 generate.addEventListener('click', () => {
     $('.alert').hide();
-    console.log("event triggered")
+    let finalPassword = "";
+    if (checkedNumber) {
+      finalPassword += numbers;
+      console.log(finalPassword);
+    }
+
 
 });
 
