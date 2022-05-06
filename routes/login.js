15f03691903f2547cd0feadc32d09e2568c9adb7
Joe Tang <joetang81@gmail.com>
2022-05-06 11:19:56 -0400

    add more comments

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ const express = require('express');
 const router = express.Router();
 const cookieSession = require('cookie-session');
 
-
+// Save user id in the session first and then redirect to /accounts route
 module.exports = (db) => {
   router.get("/:id", (req, res) => {
     req.session.user_id = req.params.id;
